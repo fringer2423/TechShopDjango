c0d7c2ebe61794b906d8a50985d94675520fae96
Vladislav Aganin <vlad_aganin33@hotmail.com>
2022-06-02 14:51:24 +0300

    Добавлен рейтинг товара

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,15 @@ import {Container} from 'react-bootstrap'
 import Header from './components/header'
 import Footer from './components/footer'
 
+import HomeScreen from './screens/HomeScreen'
+
 function App() {
     return (
         <div>
             <Header/>
             <main className="py-3">
                 <Container>
-                    <h1>Welcome</h1>
+                    <HomeScreen/>
                 </Container>
             </main>
             <Footer/>
