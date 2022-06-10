05257eb5cc1f0288891dc921b3e79a156d682903
Vladislav Aganin <vlad_aganin33@hotmail.com>
2022-06-10 17:34:16 +0300

    Реализован REDUX для управления состоянием данных и пользовательским интерфейсом

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,74 +1,83 @@
 import React, {useState, useEffect} from 'react'
+import {useDispatch, useSelector} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {Row, Col, Image, ListGroup, Button, Card} from 'react-bootstrap'
 import Rating from '../components/Rating'
-import axios from 'axios'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+import {listProductDetails} from '../actions/productActions'
 
 function ProductScreen({match}) {
-    const [product, setProduct] = useState([])
+    const dispatch = useDispatch()
+    const productDetails = useSelector(state => state.productDetails)
+    const {loading, error, product} = productDetails
 
     useEffect(() => {
-        async function fetchProduct() {
-            const {data} = await axios.get(`/api/product/${match.params.id}`)
-            setProduct(data)
-        }
+        dispatch(listProductDetails(match.params.id))
 
-        fetchProduct()
 
-    }, [])
+    }, [dispatch, match])
+
     return (
         <div>
             <Link to='/' className='btn btn-dark my-3'>На главную</Link>
-            <Row>
-                <Col md={6}>
-                    <Image src={product.image} alt={product.name} fluid/>
-                </Col>
-                <Col md={3}>
-                    <ListGroup variant='flush'>
-                        <ListGroup.Item>
-                            <h3>{product.name}</h3>
-                        </ListGroup.Item>
 
-                        <ListGroup.Item>
-                            <Rating value={product.rating} text={`${product.numReviews} отзывов`} color={'#f8e825'}/>
-                        </ListGroup.Item>
+            {loading ? <Loader/>
+                : error ? <Message variant={'danger'}>{error}</Message>
+                    :
+                    <Row>
+                        <Col md={6}>
+                            <Image src={product.image} alt={product.name} fluid/>
+                        </Col>
+                        <Col md={3}>
+                            <ListGroup variant='flush'>
+                                <ListGroup.Item>
+                                    <h3>{product.name}</h3>
+                                </ListGroup.Item>
 
-                        <ListGroup.Item>
-                            Цена: {product.price}$
-                        </ListGroup.Item>
+                                <ListGroup.Item>
+                                    <Rating value={product.rating} text={`${product.numReviews} отзывов`}
+                                            color={'#f8e825'}/>
+                                </ListGroup.Item>
 
-                        <ListGroup.Item>
-                            Описание: {product.description}
-                        </ListGroup.Item>
-                    </ListGroup>
-                </Col>
-                <Col md={3}>
-                    <Card>
-                        <ListGroup variant='flush'>
-                            <ListGroup.Item>
-                                <Row>
-                                    <Col>Цена:</Col>
-                                    <Col><strong>{product.price}$</strong></Col>
-                                </Row>
-                            </ListGroup.Item>
+                                <ListGroup.Item>
+                                    Цена: {product.price}$
+                                </ListGroup.Item>
 
-                            <ListGroup.Item>
-                                <Row>
-                                    <Col>Статус:</Col>
-                                    <Col>
-                                        {product.countInStock > 0 ? 'В наличии' : 'Нет в наличии'}
-                                    </Col>
-                                </Row>
-                            </ListGroup.Item>
+                                <ListGroup.Item>
+                                    Описание: {product.description}
+                                </ListGroup.Item>
+                            </ListGroup>
+                        </Col>
+                        <Col md={3}>
+                            <Card>
+                                <ListGroup variant='flush'>
+                                    <ListGroup.Item>
+                                        <Row>
+                                            <Col>Цена:</Col>
+                                            <Col><strong>{product.price}$</strong></Col>
+                                        </Row>
+                                    </ListGroup.Item>
 
-                            <ListGroup.Item>
-                                <Button className='btn-block btn-dark' disabled={product.countInStock == 0} type='button'>Добавить
-                                    в корзину</Button>
-                            </ListGroup.Item>
-                        </ListGroup>
-                    </Card>
-                </Col>
-            </Row>
+                                    <ListGroup.Item>
+                                        <Row>
+                                            <Col>Статус:</Col>
+                                            <Col>
+                                                {product.countInStock > 0 ? 'В наличии' : 'Нет в наличии'}
+                                            </Col>
+                                        </Row>
+                                    </ListGroup.Item>
+
+                                    <ListGroup.Item>
+                                        <Button className='btn-block btn-dark' disabled={product.countInStock == 0}
+                                                type='button'>Добавить
+                                            в корзину</Button>
+                                    </ListGroup.Item>
+                                </ListGroup>
+                            </Card>
+                        </Col>
+                    </Row>
+            }
         </div>
     )
 }
